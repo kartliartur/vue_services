be5562060aec1e715406f8fbaf25320e5f6cda1b
Artur <it.saifutdinov@gmail.com>
2020-06-25 17:35:15 +0300

    sber start

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -52,6 +52,57 @@
           </div>
       </div>
     </div>
+    <div class="nav-item sber-wrap" v-if="!isAuth">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" viewBox="0 0 50 50" width="25px"
+        height="25px" class="menu-icon active"
+        @click="changeActiveMenu('sber')">
+        <path d="M 33.539062 2.6503906 L 16 12.679688
+        L 8.2597656 8.25 C 7.7797656 8.74 7.3396875
+        9.2590625 6.9296875 9.7890625 L 16 14.990234
+        L 35.759766 3.6796875 C 35.039766 3.2896875
+        34.299062 2.9503906 33.539062 2.6503906 z M
+        38.830078 5.6191406 L 16 18.679688 L 5.1191406
+        12.449219 C 4.7791406 13.019219 4.4694531
+        13.610938 4.1894531 14.210938 L 16 20.990234
+        L 40.470703 6.9902344 C 39.950703 6.5102344
+        39.400078 6.0591406 38.830078 5.6191406 z M 42.75
+        9.3808594 L 16 24.679688 L 3.0195312
+        17.240234 C 2.8095313 17.890234 2.64
+        18.560234 2.5 19.240234
+        L 16 26.990234 L 43.960938 10.990234
+        C 43.580938 10.440234 43.18 9.9008594 42.75 9.3808594 z M
+        45.716797 14 L 16 31 L 2.046875 23 C
+        2.030875 23.333 2 23.663 2 24 C 2 36.703 12.297 47 25 47
+        C 37.703 47 48 36.703 48 24 C 48
+        20.416 47.179797 17.024 45.716797 14 z"></path>
+      </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" viewBox="0 0 50 50"
+      width="25px" height="25px" style="background: #0EB757; border-radius: 100px; padding: 5px;"
+      class="menu-icon hidden" @click="changeActiveMenu('sber')">
+        <path d="M 33.539062 2.6503906 L 16 12.679688 L 8.2597656 8.25 C 7.7797656 8.74 7.3396875
+        9.2590625 6.9296875 9.7890625 L 16 14.990234 L 35.759766 3.6796875 C 35.039766 3.2896875
+        34.299062 2.9503906 33.539062 2.6503906 z M 38.830078 5.6191406 L 16 18.679688 L 5.1191406
+        12.449219 C 4.7791406 13.019219 4.4694531 13.610938 4.1894531 14.210938 L 16 20.990234
+        L 40.470703 6.9902344 C 39.950703 6.5102344 39.400078 6.0591406
+        38.830078 5.6191406 z M 42.75
+        9.3808594 L 16 24.679688 L 3.0195312 17.240234 C 2.8095313
+        17.890234 2.64 18.560234 2.5 19.240234
+        L 16 26.990234 L 43.960938 10.990234 C 43.580938
+        10.440234 43.18 9.9008594 42.75 9.3808594 z M
+        45.716797 14 L 16 31 L 2.046875 23 C 2.030875
+        23.333 2 23.663 2 24 C 2 36.703 12.297 47 25 47
+        C 37.703 47 48 36.703 48 24 C 48 20.416 47.179797 17.024 45.716797 14 z"></path>
+      </svg>
+      <div class="nav-item-list profile-list" v-show="this.activeMenu === 'sber'">
+          <span class="close" @click="changeActiveMenu('profile')">×</span>
+          <div class="nav-item-list-item name-row">
+              <span>Аккаунт сбербанк бизнес онлайн не привязан к системе!</span>
+          </div>
+          <div class="nav-item-list-item" v-if="!isAuth">
+              <button class="sberbank-button" @click="sberAuth()">Войти c СберБизнес ID</button>
+          </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -92,6 +143,9 @@ export default {
       } else {
         this.$router.push({ path: '/' });
       }
+    },
+    sberAuth() {
+      window.location.href = `${this.$store.state.host_url}/bank_account/link`;
     },
   },
   computed: {
@@ -181,6 +235,30 @@ export default {
           font-size: 16px;
           line-height: 1.5;
           font-weight: 300;
+        }
+        & .sberbank-button {
+          display: flex;
+          align-items: center;
+          border: 1px solid #0EB757;
+          border-radius: 5px;
+          background-color: #0EB757;
+          color: #fff;
+          padding: 8px 16px;
+          text-align: center;
+          font-size: 1em;
+          cursor: pointer;
+          transition-property: box-shadow,color,background-color,border,transform;
+          transition-duration: .5s;
+          transition-timing-function: linear;
+          font-size: 1.2em;
+          font-weight: 400;
+          width: auto;
+          border: none;
+          margin: 10px auto;
+          &:hover {
+            background: #10cf62;
+            color: #fff;
+          }
         }
         &:hover { background: #f7f8f9; }
       }
